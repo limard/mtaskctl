e8429e21f254bb4b58d8e32c9216db98f6b2a0c2
limard <limard@gmail.com>
2023-10-23 14:40:46 +0800

    较大改动。。

--- mtaskctl.go
+++ mtaskctl.go
@@ -1,7 +1,6 @@
 package mtaskctl
 
 import (
-	"context"
 	"errors"
 	"sync"
 )
@@ -11,13 +10,16 @@ var Canceled = errors.New("canceled")
 const CHANNEL_NUMBER = 8
 
 type MTaskCtl struct {
-	n      [CHANNEL_NUMBER]int        // 当前并发数量
-	max    [CHANNEL_NUMBER]int        // 最大并发数量
-	ch     [CHANNEL_NUMBER]chan error // 控制数量，cancel后error为非空
-	mu     sync.Mutex                 //
-	e      error                      // 运行/取消状态
-	pa     bool                       // pause 暂停状态
-	resume chan struct{}              // 恢复信号
+	max [CHANNEL_NUMBER]int // 最大并发数量
+
+	mtxN sync.Mutex
+	n    [CHANNEL_NUMBER]int           // 当前并发数量
+	ch   [CHANNEL_NUMBER]chan struct{} // 控制数量，cancel后error为非空
+
+	mtxPause  sync.Mutex    //
+	pause     bool          // pause 暂停状态
+	pauseChan chan struct{} // 暂停时有chan造成阻塞，恢复后close解除阻塞
+	cancel    error         // 运行/取消状态
 }
 
 func NewTaskCtl(max []int) *MTaskCtl {
@@ -26,99 +28,108 @@ func NewTaskCtl(max []int) *MTaskCtl {
 	}
 
 	task := &MTaskCtl{
-		resume: make(chan struct{}),
+		pause:     true,
+		pauseChan: make(chan struct{}),
 	}
 	for i, v := range max {
 		task.max[i] = v
-		task.ch[i] = make(chan error, v)
+		task.ch[i] = make(chan struct{}, v)
 		for j := 0; j < v; j++ {
-			task.ch[i] <- nil
+			task.ch[i] <- struct{}{}
 		}
 	}
+	task.Resume() // 默认不暂停
 	return task
 }
 
-// 代理 Start Done Wait 的功能，可以在callback中调用 Check
-func (t *MTaskCtl) Do(cb func(i, channel int, cancel context.CancelFunc)) {
-	ctx, cancel := context.WithCancel(context.Background())
-	for i := 0; ; i++ {
-		channel, e := t.Start()
+func (t *MTaskCtl) Do(cb func(index, channel int)) {
+	for index := 0; ; index++ {
+		channel, e := t.assign()
 		if e != nil {
 			break
 		}
-		if ctx.Err() != nil {
-			t.Done(channel)
-			break
-		}
-		go func(i, channel int) {
-			cb(i, channel, cancel)
-			t.Done(channel)
-		}(i, channel)
-	}
-	t.Wait()
 
-	// cancel()写在这儿没特殊意义，但是不写的话会有警告
-	// the cancel function is not used on all paths (possible context leak)lostcancel
-	cancel()
+		go func(i, channel int) {
+			cb(i, channel)
+			t.done(channel)
+		}(index, channel)
+	}
+	t.wait()
 }
 
-// Start在运行goroutine之前执行，检查是否可以开始。
+// 分配一个可用的channel
 // nil为正常继续，error为终止（Cancel），并发数量不足或pause时将阻塞
-func (t *MTaskCtl) Start() (index int, e error) {
-	if t.e != nil {
-		return 0, t.e
+func (t *MTaskCtl) assign() (channel int, e error) {
+	// 如果已经取消，则直接返回
+	if e = t.Check(); e != nil {
+		return 0, e
 	}
 
+	// 等待空闲channel，或者close
+	var ok bool
 	select {
-	case e = <-t.ch[0]:
-		index = 0
-	case e = <-t.ch[1]:
-		index = 1
-	case e = <-t.ch[2]:
-		index = 2
-	case e = <-t.ch[3]:
-		index = 3
-	case e = <-t.ch[4]:
-		index = 4
-	case e = <-t.ch[5]:
-		index = 5
-	case e = <-t.ch[6]:
-		index = 6
-	case e = <-t.ch[7]:
-		index = 7
+	case _, ok = <-t.ch[0]:
+		channel = 0
+	case _, ok = <-t.ch[1]:
+		channel = 1
+	case _, ok = <-t.ch[2]:
+		channel = 2
+	case _, ok = <-t.ch[3]:
+		channel = 3
+	case _, ok = <-t.ch[4]:
+		channel = 4
+	case _, ok = <-t.ch[5]:
+		channel = 5
+	case _, ok = <-t.ch[6]:
+		channel = 6
+	case _, ok = <-t.ch[7]:
+		channel = 7
 	}
-	if e == nil {
-		t.n[index]++
+
+	// 如果已经取消，则直接返回
+	if e = t.Check(); e != nil {
+		return 0, e
 	}
+
+	// 如果通道关闭也返回
+	if !ok {
+		return 0, Canceled
+	}
+
+	t.mtxN.Lock()
+	t.n[channel]++
+	t.mtxN.Unlock()
 	return
+}
+
+// 完成后插入新元素
+func (t *MTaskCtl) done(index int) {
+	t.mtxN.Lock()
+	t.n[index]--
+	t.mtxN.Unlock()
+
+	if len(t.ch[index]) < cap(t.ch[index]) {
+		t.ch[index] <- struct{}{}
+	}
+}
+
+// 等待所有线程执行完
+func (t *MTaskCtl) wait() {
+	for i := 0; i < CHANNEL_NUMBER; i++ {
+		for t.n[i] > 0 {
+			<-t.ch[i]
+		}
+	}
 }
 
 // Check在运行routine过程中执行，检查是否继续。
 // nil为正常继续，error为终止（Cancel），pause时阻塞
 func (t *MTaskCtl) Check() error {
-	if t.pa {
-		<-t.resume
-	}
-	return t.e
-}
+	// 暂停时阻塞，恢复(close)后解除阻塞
+	<-t.pauseChan
 
-// Done线程完成后回收资源。Check为error时，也需要调用Done。
-func (t *MTaskCtl) Done(index int) {
-	t.mu.Lock()
-	t.n[index]--
-	if !t.pa && len(t.ch[index]) < cap(t.ch[index]) {
-		t.ch[index] <- t.e
-	}
-	t.mu.Unlock()
-}
-
-// Wait等待所有线程执行完
-func (t *MTaskCtl) Wait() {
-	for i := 0; i < CHANNEL_NUMBER; i++ {
-		for t.n[i] != 0 {
-			<-t.ch[i]
-		}
-	}
+	// 如果任务取消，则cancel为非nil
+	return t.cancel
 }
 
 // Close关闭
@@ -129,52 +140,32 @@ func (t *MTaskCtl) Close() {
 			t.ch[i] = nil
 		}
 	}
-	if t.resume != nil {
-		close(t.resume)
-		t.resume = nil
-	}
 }
 
-// TaskCancel取消任务，cause为选填的取消原因
-func (t *MTaskCtl) TaskCancel(cause error) {
+// 取消任务，cause为选填的取消原因
+func (t *MTaskCtl) Cancel(cause error) {
 	if cause != nil {
-		t.e = cause
+		t.cancel = cause
 	} else {
-		t.e = Canceled
+		t.cancel = Canceled
 	}
-	if t.pa {
-		t.TaskResume()
-	}
+	t.Resume()
 }
 
-// TaskPause暂停任务
-func (t *MTaskCtl) TaskPause() {
-	t.mu.Lock()
-	t.pa = true
-	for i := 0; i < CHANNEL_NUMBER; i++ {
-		for j := t.n[i]; j < t.max[i]; j++ {
-			<-t.ch[i]
-		}
-	}
-	t.mu.Unlock()
+// 暂停任务
+func (t *MTaskCtl) Pause() {
+	t.mtxPause.Lock()
+	t.pause = true
+	t.pauseChan = make(chan struct{})
+	t.mtxPause.Unlock()
 }
 
-// TaskResume恢复任务
-func (t *MTaskCtl) TaskResume() {
-	t.mu.Lock()
-	t.pa = false
-	for i := 0; i < CHANNEL_NUMBER; i++ {
-		for j := t.n[i]; j < t.max[i]; j++ {
-			t.ch[i] <- t.e
-		}
+// 恢复任务
+func (t *MTaskCtl) Resume() {
+	t.mtxPause.Lock()
+	if t.pause {
+		close(t.pauseChan)
 	}
-	for {
-		select {
-		case t.resume <- struct{}{}:
-		default:
-			goto end
-		}
-	}
-end:
-	t.mu.Unlock()
+	t.pause = false
+	t.mtxPause.Unlock()
 }
--- mtaskctl_test.go
+++ mtaskctl_test.go
@@ -1,91 +1,80 @@
 package mtaskctl
 
 import (
-	"context"
 	"fmt"
 	"sync"
 	"testing"
 	"time"
 )
 
-func Test_Task(t *testing.T) {
-	start := time.Now()
-	mu := sync.Mutex{}
-	pf := func(a ...any) {
-		mu.Lock()
-		fmt.Printf("%6.2fs ", float64(time.Now().Sub(start).Milliseconds())/1000)
-		fmt.Println(a...)
-		mu.Unlock()
-	}
-
+func Test_TaskDo(t *testing.T) {
+	pf := newpf()
 	ctl := NewTaskCtl([]int{2})
 
 	go func() {
 		time.Sleep(2 * time.Second)
-		pf("======= pause1 =======")
-		ctl.TaskPause()
-		pf("======= pause2 =======")
+		pf.p("======= pause =======")
+		ctl.Pause()
+		// pf.p("======= pause =======")
 
 		time.Sleep(8 * time.Second)
-		pf("======= resume1 =======")
-		ctl.TaskResume()
-		pf("======= resume2 =======")
+		pf.p("======= resume =======")
+		ctl.Resume()
+		// pf.p("======= resume =======")
 
 		// time.Sleep(8 * time.Second) // start_error
 		time.Sleep(5 * time.Second) // check_error
-		pf("======= pause1 =======")
-		ctl.TaskPause()
-		pf("======= pause2 =======")
+		pf.p("======= pause =======")
+		ctl.Pause()
+		// pf.p("======= pause =======")
 
 		time.Sleep(8 * time.Second)
-		pf("======= cancel1 =======")
-		ctl.TaskCancel(nil)
-		pf("======= cancel2 =======")
+		pf.p("======= cancel =======")
+		ctl.Cancel(nil)
+		pf.p("======= cancel =======")
 	}()
 
-	for i := 0; i < 10; i++ {
-		pf(i, "start1")
-		index, e := ctl.Start()
-		if e != nil {
-			pf(i, "start-error")
-			break
-		}
-		pf(i, "\tstart2")
-
-		go func(i int) {
-			defer func() {
-				pf(i, "\t\t\tdone")
-				ctl.Done(index)
-			}()
-
-			time.Sleep(3 * time.Second)
-			pf(i, "\t\tcheck1")
-			if ctl.Check() != nil {
-				pf(i, "\t\tcheck-error")
-				return
-			}
-			pf(i, "\t\tcheck2")
-
-			time.Sleep(3 * time.Second)
-		}(i)
-	}
-
-	pf("======= wait =======")
-	ctl.Wait()
-	ctl.Close()
-}
-
-func Test_do(t *testing.T) {
-	ctl := NewTaskCtl([]int{2, 3})
-
-	ctl.Do(func(n, channel int, cancel context.CancelFunc) {
-		fmt.Println(n, channel, "start")
-		if n == 10 {
-			cancel()
-			fmt.Println("cancel")
+	ctl.Do(func(index, channel int) {
+		pf.p("index:", index, " channel:", channel, "start")
+		if index == 10 {
+			ctl.Cancel(nil)
+			pf.p("cancel")
 			return
 		}
+
 		time.Sleep(1 * time.Second)
-		fmt.Println(n, channel, "       --- end")
+
+		if e := ctl.Check(); e != nil {
+			pf.p("check out", e.Error())
+			return
+		}
+
+		time.Sleep(1 * time.Second)
+
+		if e := ctl.Check(); e != nil {
+			pf.p("check out", e.Error())
+			return
+		}
+
+		pf.p("index:", index, " channel:", channel, "--- end")
 	})
+	pf.p("Finished")
+}
+
+type pf struct {
+	start time.Time
+	mu    sync.Mutex
+}
+
+func newpf() *pf {
+	return &pf{
+		start: time.Now(),
+	}
+}
+
+func (t *pf) p(a ...any) {
+	t.mu.Lock()
+	fmt.Printf("%6.2fs ", float64(time.Now().Sub(t.start).Milliseconds())/1000)
+	fmt.Println(a...)
+	t.mu.Unlock()
 }
